b362593d842916e57e41a2ccb8b79bc082dcbcf8
Sagar <sagar.jobs@outlook.com>
2023-08-07 17:12:57 +1000

    first commit

---
+++ src/test/java/com/spring/cloud/azureblob/AzureBlobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.azureblob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBlobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
